b7b61143efde28c1b0bfe6aa27415a64c612cb9d
MrRale <mrrafas29@hotmail.com>
2021-08-09 02:15:55 -0500

    Subiendo los primeros archivos

---
+++ frontend/src/app/enum/OrderStatus.ts
@@ -0,0 +1,5 @@
+export enum OrderStatus {
+    "Nuevo",
+    "Termiando",
+    "Cancelado"
+}
---
+++ frontend/src/app/models/Order.ts
@@ -0,0 +1,8 @@
+export class Order {
+    orderId: number;
+    buyerName: string;
+    buyerPhone: string;
+    buyerAddress: string;
+    orderAmount: string;
+    orderStatus: string;
+}
